c56b25b1ef5df99d8f28893652aa2272c34d75f0
Kurt Madsen <kurt@crowdbotics.com>
2022-12-27 22:54:23 -0800

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "samz_38038",
   url: "https://samz_38038.botics.co",
   api: "https://samz_38038.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
